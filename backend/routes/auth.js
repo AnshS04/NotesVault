8990775ee61268b1265628297f126fe03999fb71
AnshSachdeva23 <sachdevaansh2002@gmail.com>
2023-09-03 20:51:03 +0530

    thunder client collections and manage requests

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,36 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
-    obj = {
-        a: 'this',
-        number: 10
+// create a user using: post "/api/auth"/ , it doesnt require auth
+router.post('/createuser', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be 5 characters long').isLength({ min: 5 })
+], async (req, res) => {
+
+    // if errors, send bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
-    res.json(obj)
+
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Email already in use" })
+        }
+        user = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email
+        })
+        res.json({ user })
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
 })
 
 module.exports = router
